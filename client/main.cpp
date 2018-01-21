07fe65828b41bf3068200665f99d23a8b9221a3f
DevForgeNetwork <DevForgeNetwork@gmail.com>
2018-01-21 15:52:47 -0800

    Client/Server exercises sfml-network linking
    - Add sfml-network dependencies to sfml-props

--- client/main.cpp
+++ client/main.cpp
@@ -5,6 +5,30 @@
 
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+
+class Connection
+{
+public:
+	Connection()
+		: m_isConnected(false)
+	{
+		m_socketConnection.setBlocking(false);
+	}
+
+	sf::TcpSocket& GetSocket() { return m_socketConnection; }
+	bool IsConnnected() { return m_isConnected; }
+
+	// These two are separate functions for verbosity.
+	void Disconnect() { m_isConnected = false; }
+	void Connect() { m_isConnected = true; }
+
+private:
+	sf::TcpSocket m_socketConnection;
+	bool m_isConnected;
+};
+
+//-----------------------------------------------------------------------------------
 
 int main()
 {
--- server/main.cpp
+++ server/main.cpp
@@ -9,27 +9,28 @@
 #include <memory>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
 
-//class Connection
-//{
-//public:
-//	Connection()
-//		: m_isConnected(false)
-//	{
-//		m_socketConnection.setBlocking(false);
-//	}
-//
-//	sf::TcpSocket& GetSocket() { return m_socketConnection; }
-//	bool IsConnnected() { return m_isConnected; }
-//
-//	// These two are separate functions for verbosity.
-//	void Disconnect() { m_isConnected = false; }
-//	void Connect() { m_isConnected = true; }
-//
-//private:
-//	sf::TcpSocket m_socketConnection;
-//	bool m_isConnected;
-//};
+class Connection
+{
+public:
+	Connection()
+		: m_isConnected(false)
+	{
+		m_socketConnection.setBlocking(false);
+	}
+
+	sf::TcpSocket& GetSocket() { return m_socketConnection; }
+	bool IsConnnected() { return m_isConnected; }
+
+	// These two are separate functions for verbosity.
+	void Disconnect() { m_isConnected = false; }
+	void Connect() { m_isConnected = true; }
+
+private:
+	sf::TcpSocket m_socketConnection;
+	bool m_isConnected;
+};
 
 //-----------------------------------------------------------------------------------
 
@@ -74,6 +75,7 @@ namespace {
 
 int main()
 {
+	Connection c;
 	sf::Window window(sf::VideoMode(800, 600), "My window");
 	// Just makes sure SFML System is linking properly
 	sf::Time t1 = sf::seconds(0.1f);
